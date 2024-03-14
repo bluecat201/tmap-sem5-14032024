e16d9974b1d023885e040ee4bbabf087dcae0bd8
Bluecat201 <kopahnojenic@gmail.com>
2024-03-14 19:11:22 +0100

    ukol

--- app/src/main/java/com/example/sem5_14032024/MainActivity.kt
+++ app/src/main/java/com/example/sem5_14032024/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Sem514032024Theme {
+                casf
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
