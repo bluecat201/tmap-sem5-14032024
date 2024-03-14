73604640a18f06df7267da010719ce23b3e4f698
Bluecat201 <kopahnojenic@gmail.com>
2024-03-14 19:09:15 +0100

    ukol

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "sem5-14032024"
+include(":app")
+ 
